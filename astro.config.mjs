f9aa1c666299e2d5b5d97d59f7e5652f7a05afe0
MarcusQuirino <marcusfilipus@gmail.com>
2025-09-29 19:35:33 -0300

    :3

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,24 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://southlikesoftware.com',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/obrigado')
+    })
+  ]
 });
